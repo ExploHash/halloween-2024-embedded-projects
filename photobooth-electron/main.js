14ca3b7d1ccbf2067a80816cfbfcec1f9e0fa11a
ExploHash <jbtwaalf@gmail.com>
2024-10-27 21:33:38 +0100

    feat: added zombie mode and fixed pins

--- photobooth-electron/main.js
+++ photobooth-electron/main.js
@@ -1,66 +1,68 @@
 // main.js
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const fs = require('fs');
-const dayjs = require('dayjs');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const fs = require("fs");
+const dayjs = require("dayjs");
 
 // Run gpioget 0 21 every 10ms
-const exec = require('child_process').exec;
+const exec = require("child_process").exec;
 setInterval(() => {
-    exec('gpioget 0 21', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
+  exec("gpioget 2 13", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
 
-        if (stdout.trim() === '1') {
-            BrowserWindow.getAllWindows().forEach((window) => {
-                window.webContents.send('button-pressed');
-            });
-        }
-    });
+    if (stdout.trim() === "1") {
+      BrowserWindow.getAllWindows().forEach((window) => {
+        window.webContents.send("button-pressed");
+      });
+    }
+  });
 }, 10);
 
-
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 640,
-        height: 480,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            contextIsolation: true,
-            enableRemoteModule: false,
-        },
-        // fullscreen: true
-    });
+  const win = new BrowserWindow({
+    width: 640,
+    height: 480,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      enableRemoteModule: false,
+    },
+    // fullscreen: true
+  });
 
-    win.loadFile('index.html');
+  win.loadFile("index.html");
 }
 
 // Listen for image saving requests
-ipcMain.on('save-image', (event, imageData) => {
-    const base64Data = imageData.replace(/^data:image\/png;base64,/, '');
-    const buffer = Buffer.from(base64Data, 'base64');
+ipcMain.on("save-image", (event, imageData) => {
+  const base64Data = imageData.replace(/^data:image\/png;base64,/, "");
+  const buffer = Buffer.from(base64Data, "base64");
 
-    const picturesPath = path.join(app.getPath('pictures'), "photobooth-" + dayjs().format('YYYY-MM-DD-HH-mm-ss') + '.png');
+  const picturesPath = path.join(
+    app.getPath("pictures"),
+    "photobooth-" + dayjs().format("YYYY-MM-DD-HH-mm-ss") + ".png",
+  );
 
-    fs.writeFile(picturesPath, buffer, (err) => {
-        if (err) {
-            console.error('Error saving image:', err);
-            event.reply('image-saved', 'Error saving image');
-        } else {
-            console.log('Image saved to:', picturesPath);
-            event.reply('image-saved', 'Image saved successfully');
-        }
-    });
+  fs.writeFile(picturesPath, buffer, (err) => {
+    if (err) {
+      console.error("Error saving image:", err);
+      event.reply("image-saved", "Error saving image");
+    } else {
+      console.log("Image saved to:", picturesPath);
+      event.reply("image-saved", "Image saved successfully");
+    }
+  });
 });
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
--- photobooth-electron/renderer.js
+++ photobooth-electron/renderer.js
@@ -12,249 +12,294 @@ let isProcessingImage = false;
 let showImageTimeout;
 let isShowingPreparing = false;
 let isUploading = false;
+let isUploadingFailed = false;
 
 async function initCamera() {
-    const video = document.getElementById('video');
-    const stream = await navigator.mediaDevices.getUserMedia({
-        video: {
-            width: 1920,
-            height: 1080,
-            frameRate: 30
-        }
-    });
-    video.srcObject = stream;
+  const video = document.getElementById("video");
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: {m
+      width: 1920,
+      height: 1080,
+      frameRate: 30,
+    },
+  });
+  video.srcObject = stream;
 }
 
 function buttonPressed() {
-    // If we are already taking a picture or showing the preparing screen, return
-    if (isTakingPicture || isShowingPreparing || isShowingImage) {
-        return;
-    }
+  // If we are already taking a picture or showing the preparing screen, return
+  if (isTakingPicture || isShowingPreparing || isShowingImage) {
+    return;
+  }
 
-    // If we are showing the end screen, hide it
-    if (isInEndScreen) {
-        hideEndScreen();
-        showFirstInstructions();
-        return;
-    }
+  // If we are showing the end screen, hide it
+  if (isInEndScreen) {
+    hideEndScreen();
+    showFirstInstructions();
+    return;
+  }
 
-    startCountdown();
+  startCountdown();
 }
 
 function startCountdown() {
-    isTakingPicture = true;
+  isTakingPicture = true;
 
-    // Reset and show the countdown
-    let countdown = countdownFrom;
-    showCountdown();
-    updateCountdown(countdown);
+  // Reset and show the countdown
+  let countdown = countdownFrom;
+  showCountdown();
+  updateCountdown(countdown);
 
-    // Hide first and every instructions
-    hideFirstInstructions();
-    hideEveryInstructions();
+  // Hide first and every instructions
+  hideFirstInstructions();
+  hideEveryInstructions();
 
-    const interval = setInterval(() => {
-        updateCountdown(--countdown);
+  const interval = setInterval(() => {
+    updateCountdown(--countdown);
 
-        if (countdown <= 0) {
-            clearInterval(interval);
-            setTimeout(() => {
-                hideCountdown();
-                captureImage();
-                isTakingPicture = false;
-            }, 50);
-        }
-    }, 1000);
+    // Activate zombie mode on third picture on the 2
+    if (countdown === 2 && capturedImages.length === 2) {
+      // setTimeout(activateZombieMode, 500);
+    }
+
+    if (countdown <= 0) {
+      clearInterval(interval);
+      setTimeout(() => {
+        hideCountdown();
+        captureImage();
+        isTakingPicture = false;
+      }, 50);
+    }
+  }, 1000);
+}
+
+function activateZombieMode() {
+  // Stop countdown after
+  hideCountdown();
+
+  // First show zombie image
+  showZombieImage();
+
+  // After 200ms take a photo
+  setTimeout(() => {
+    captureImage();
+    isTakingPicture = false;
+  }, 200);
+
+  // After 300ms hide image
+  setTimeout(hideZombieImage, 300);
 }
 
 function captureImage() {
-    // Draw the video frame to the canvas
-    const video = document.getElementById('video');
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  // Draw the video frame to the canvas
+  const video = document.getElementById("video");
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext("2d");
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    // Convert canvas to data URL and display it
-    const dataURL = canvas.toDataURL('image/png');
-    showImage(dataURL);
-    
-    // Convert the captured image to a Blob
-    canvas.toBlob((blob) => {
-        if (blob) {
-            capturedImages.push(blob); // Add the Blob to the array
+  // Convert canvas to data URL and display it
+  const dataURL = canvas.toDataURL("image/png");
+  showImage(dataURL);
 
-            // If we have 3 images, upload them
-            if (capturedImages.length === 3) {
-                uploadImages(capturedImages);
-                capturedImages.length = 0; // Clear the array after uploading
-            }
-        }
-    }, 'image/png');
+  // Convert the captured image to a Blob
+  canvas.toBlob((blob) => {
+    if (blob) {
+      capturedImages.push(blob); // Add the Blob to the array
 
-    window.electron.saveImage(dataURL);
+      // If we have 3 images, upload them
+      if (capturedImages.length === 3) {
+        uploadImages(capturedImages);
+        capturedImages.length = 0; // Clear the array after uploading
+      }
+    }
+  }, "image/png");
+
+  window.electron.saveImage(dataURL);
 }
 
 function showImage(dataURL) {
-    isShowingImage = true;
-    showOverlayImage(dataURL);
+  isShowingImage = true;
+  showOverlayImage(dataURL);
 
-    // Hide the image after 3 seconds
-    showImageTimeout = setTimeout(() => {
-        hideOverlayImage();
+  // Hide the image after 3 seconds
+  showImageTimeout = setTimeout(() => {
+    hideOverlayImage();
 
-        // If we have captured 3 images, show the preparing screen
-        if(capturedImages.length === 0) {
-            if (isUploading) {
-                showPreparingInstructions();
-            } else {
-                showEndScreen();
-            }
-        } else {
-            showEveryInstructions();
-        }
-    }, showImageMs);
+    // If we have captured 3 images, show the preparing screen
+    if (capturedImages.length === 0) {
+      if (isUploading) {
+        showPreparingInstructions();
+      } else {
+        showEndScreen();
+      }
+    } else {
+      showEveryInstructions();
+    }
+  }, showImageMs);
 }
 
 function showEndScreen() {
-    const endInstructions = document.querySelector('#end-instructions');
-    endInstructions.style.opacity = 1;
-    isInEndScreen = true;
+  const endInstructions = document.querySelector("#end-instructions");
+  endInstructions.style.opacity = 1;
+  isInEndScreen = true;
 
-    endCountdownInterval = setInterval(() => {
-        updateEndScreenCountdown();
+  if (isUploadingFailed) {
+    document.getElementById("qr-code").innerHTML =
+      "<b>Failed to upload! Ask us afterwards for the photos :)</b>";
+  }
 
-        if (endscreenCountdown <= 0) {
-            hideEndScreen();
-            showFirstInstructions();
-        }
-    }, 1000);
+  endCountdownInterval = setInterval(() => {
+    updateEndScreenCountdown();
+
+    if (endscreenCountdown <= 0) {
+      hideEndScreen();
+      showFirstInstructions();
+    }
+  }, 1000);
 }
 
 function showOverlayImage(dataURL) {
-    const imageDisplay = document.querySelector('#overlayImage');
-    imageDisplay.style.display = 'block';
-    imageDisplay.src = dataURL;
+  const imageDisplay = document.querySelector("#overlayImage");
+  imageDisplay.style.display = "block";
+  imageDisplay.src = dataURL;
 }
 
 function hideOverlayImage() {
-    clearTimeout(showImageTimeout);
-    const imageDisplay = document.querySelector('#overlayImage');
-    imageDisplay.style.display = 'none';
-    isShowingImage = false;
+  clearTimeout(showImageTimeout);
+  const imageDisplay = document.querySelector("#overlayImage");
+  imageDisplay.style.display = "none";
+  isShowingImage = false;
+}
+
+function showZombieImage() {
+  const imageDisplay = document.querySelector("#zombieImage");
+  imageDisplay.style.display = "block";
+}
+
+function hideZombieImage() {
+  const imageDisplay = document.querySelector("#zombieImage");
+  imageDisplay.style.display = "none";
 }
 
 function showFirstInstructions() {
-    const firstInstructions = document.querySelector('#first-instructions');
-    firstInstructions.style.opacity = 1;
+  const firstInstructions = document.querySelector("#first-instructions");
+  firstInstructions.style.opacity = 1;
 }
 
 function hideFirstInstructions() {
-    const firstInstructions = document.querySelector('#first-instructions');
-    firstInstructions.style.opacity = 0;
+  const firstInstructions = document.querySelector("#first-instructions");
+  firstInstructions.style.opacity = 0;
 }
 
 function hideEndScreen() {
-    const endInstructions = document.querySelector('#end-instructions');
-    endInstructions.style.opacity = 0;
-    isInEndScreen = false;
-    clearInterval(endCountdownInterval);
-    // Clear the qr code
-    document.getElementById("qr-code").innerHTML = '';
-    endscreenCountdown = 30;
+  const endInstructions = document.querySelector("#end-instructions");
+  endInstructions.style.opacity = 0;
+  isInEndScreen = false;
+  clearInterval(endCountdownInterval);
+  // Clear the qr code
+  document.getElementById("qr-code").innerHTML = "";
+  endscreenCountdown = 30;
 }
 
 function showCountdown() {
-    const countdownDisplay = document.getElementById('countdown');
-    countdownDisplay.style.display = 'block';
+  const countdownDisplay = document.getElementById("countdown");
+  countdownDisplay.style.display = "block";
 }
 
 function hideCountdown() {
-    const countdownDisplay = document.getElementById('countdown');
-    countdownDisplay.style.display = 'none';
+  const countdownDisplay = document.getElementById("countdown");
+  countdownDisplay.style.display = "none";
 }
 
 function updateCountdown(countdown) {
-    const countdownDisplay = document.getElementById('countdown');
-    countdownDisplay.innerText = countdown;
+  const countdownDisplay = document.getElementById("countdown");
+  countdownDisplay.innerText = countdown;
 }
 
 function hideEveryInstructions() {
-    const everyInstructions = document.querySelector('#every-instructions');
-    everyInstructions.style.opacity = 0;
+  const everyInstructions = document.querySelector("#every-instructions");
+  everyInstructions.style.opacity = 0;
 }
 
 function showEveryInstructions() {
-    const everyInstructions = document.querySelector('#every-instructions');
-    everyInstructions.style.opacity = 1;
-    everyInstructions.innerHTML = `<h1>Press the button to start the countdown (${capturedImages.length + 1}/3)</h1>`;
+  const everyInstructions = document.querySelector("#every-instructions");
+  everyInstructions.style.opacity = 1;
+  everyInstructions.innerHTML = `<h1>Press the button to start the countdown (${capturedImages.length + 1}/3)</h1>`;
 }
 
 function showPreparingInstructions() {
-    const preparingInstructions = document.querySelector('#preparing-instructions');
-    preparingInstructions.style.opacity = 1;
-    isShowingPreparing = true;
+  const preparingInstructions = document.querySelector(
+    "#preparing-instructions",
+  );
+  preparingInstructions.style.opacity = 1;
+  isShowingPreparing = true;
 }
 
 function hidePreparingInstructions() {
-    const preparingInstructions = document.querySelector('#preparing-instructions');
-    preparingInstructions.style.opacity = 0;
-    isShowingPreparing = false;
+  const preparingInstructions = document.querySelector(
+    "#preparing-instructions",
+  );
+  preparingInstructions.style.opacity = 0;
+  isShowingPreparing = false;
 }
 
 function updateEndScreenCountdown() {
-    const restartCountdown = document.querySelector('#restart-countdown');
-    restartCountdown.innerText = --endscreenCountdown;
+  const restartCountdown = document.querySelector("#restart-countdown");
+  restartCountdown.innerText = --endscreenCountdown;
 }
 
 // Function to upload images to a specified URL using FormData
 async function uploadImages(images) {
-    isUploading = true;
-    const formData = new FormData();
+  isUploading = true;
+  isUploadingFailed = false;
+  const formData = new FormData();
 
-    images.forEach((image, index) => {
-        formData.append('images', image, `image-${index}.png`);
+  images.forEach((image, index) => {
+    formData.append("images", image, `image-${index}.png`);
+  });
+
+  try {
+    const response = await fetch(apiUrl + "/upload", {
+      method: "POST",
+      body: formData,
     });
 
-    try {
-        const response = await fetch(apiUrl + "/upload", {
-            method: 'POST',
-            body: formData
-        });
-
-        if (response.ok) {
-            console.log('Images uploaded successfully');
-            // Update qr code with received uuid
-            const data = await response.json();
-            document.getElementById("qr-code").innerHTML = '';
-            new QRCode(document.getElementById("qr-code"), `${apiUrl}/view/${data.uuid}`);
-        } else {
-            console.error('Error uploading images');
-        }
-        isUploading = false;
-
-        if (isShowingPreparing) {
-            isShowingPreparing = false;
-            showEndScreen();
-
-            const preparingInstructions = document.querySelector('#preparing-instructions');
-            preparingInstructions.style.opacity = 0;
-        }
-    } catch (error) {
-        console.error('Error uploading images:', error);
+    if (response.ok) {
+      console.log("Images uploaded successfully");
+      // Update qr code with received uuid
+      const data = await response.json();
+      document.getElementById("qr-code").innerHTML = "";
+      new QRCode(
+        document.getElementById("qr-code"),
+        `${apiUrl}/view/${data.uuid}`,
+      );
+    } else {
+      console.error("Error uploading images");
+      isUploadingFailed = true;
     }
+  } catch (error) {
+    console.error("Error uploading images:", error);
+    isUploadingFailed = true;
+  }
+  isUploading = false;
+
+  if (isShowingPreparing) {
+    hidePreparingInstructions();
+    showEndScreen();
+  }
 }
 
 // Listen for image saved response
 window.electron.onImageSaved((event, message) => {
-    console.log(message);
+  console.log(message);
 });
 
 // Listen for the start countdown signal from the main process
 window.electron.onButtonPressed(() => {
-    buttonPressed();
+  buttonPressed();
 });
 
-video.addEventListener('click', buttonPressed);
-window.addEventListener('load', initCamera);
+video.addEventListener("click", buttonPressed);
+window.addEventListener("load", initCamera);
